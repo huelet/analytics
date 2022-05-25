cc1e6d910da1f6732da42f7ea4919ca2a5edd59a
Levi Richardson <litbelb1@outlook.com>
2022-05-25 16:33:48 +0000

    🎉 basic fiber app setup

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	app := fiber.New()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	app.Listen(fmt.Sprintf(":%s", os.Getenv("PORT")))
+}
